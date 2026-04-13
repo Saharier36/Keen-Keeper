fd57b908d6da79290ea3fa5636a45ed556f43b61
Saharier Omi <gsaharier761@gmail.com>
2026-04-13 22:32:56 +0600

    Add assets, JSON file, and multiple packages to the project

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,18 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  // https://randomuser.me/api/portraits/men/32.jpg
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        port: "",
+        pathname: "/api/portraits/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
